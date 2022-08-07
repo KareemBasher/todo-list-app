af4721d0af901e5cefefb1524c053d531087931c
kareembasher <kareem.basher@gmail.com>
2022-08-07 22:22:31 +0200

    Created a notebook component with another Entries component inside.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { NoteBook } from './NoteBook';
 
 function App() {
   return (
-    <div className="">
-      
+    <div className="dark:bg-zinc-900 dark:text-white h-screen flex">
+      <div className='w-1/3'>
+        
+      </div>
+      <NoteBook />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/App.js"],
+  content: ["./src/App.js", "./src/NoteBook.jsx", "./src/Entries.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'midnight': '#201F1E',
+        'lineRed': '#D71324',
+        'lineBlue': '#133B56',
+        'altLineRed': '#7c000a',
+      },
+    },
   },
   plugins: [],
 }
