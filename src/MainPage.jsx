3af3dc084da2c514305038c0088f3e069203baa2
kareembasher <kareem.basher@gmail.com>
2022-08-10 17:34:48 +0200

    Made a GET request to get the preferred mode and a PATCH request to change that preference.

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -3,14 +3,33 @@ import { NoteBook } from './NoteBook';
 import { SidePanel } from './SidePanel';
 import { useParams } from "react-router-dom";
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const MainPage = () => {
     const [darkMode, setDarkMode] = useState(true);
     const params = useParams();
     const userName = params.user;
 
+    useEffect(() => {
+      fetch("http://localhost:3001/users/1")
+      .then(result => {
+        return result.json()
+      })
+      .then(data => {
+        setDarkMode(data.darkMode)
+      })
+    }, [])
+
     const toggleDarkMode = () => {
         setDarkMode(!darkMode);
+
+        fetch("http://localhost:3001/users/1", {
+          method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'darkMode': !darkMode })
+        })
     }
 
     const darkModeStyles = 'bg-zinc-900 text-white';
