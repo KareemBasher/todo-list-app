028f19f09be6a64b61f68eaeb3a1a1a238267240
kareembasher <kareem.basher@gmail.com>
2022-08-12 21:04:31 +0200

    Finished the edit functionality.

--- src/Entries.jsx
+++ src/Entries.jsx
@@ -5,12 +5,15 @@ import { useParams } from "react-router-dom";
 import { useEffect } from 'react';
 import ClipLoader from "react-spinners/ClimbingBoxLoader";
 import { v4 as uuidv4 } from 'uuid';
+import { FiPlus } from 'react-icons/fi';
 
 export const Entries = props => {
     const { darkMode } = props;
     const [ listData, setListData ] = useState(null);
     const [ addDisabled, setAddDisabled ] = useState(false);
     const [ addState, setAddState ] = useState(false);
+    const [ editState, setEditState ] = useState(false);
+    const [ editDisabled, setEditDisabled ] = useState(false);
     const [ typedData, setTypedData ] = useState({ content: '' });
     const params = useParams();
     const userId = params.user;
@@ -66,9 +69,35 @@ export const Entries = props => {
         }))
     }
 
+    const changeEditInput = e => {
+        const targetId = e.target.dataset.id;
+        let newData = [...listData];
+        const index = newData.findIndex(item => item.id === targetId);
+
+        newData[index].content = e.target.value;
+        newData[index].checked = false;
+        setListData(newData);
+    }
+
     const handleAddButton = () => {
-        setAddDisabled(true)
-        setAddState(true);
+        if (addDisabled) {
+            setAddDisabled(false)
+            setTypedData({ content: '' });
+            setAddState(false);
+        } else {
+            setAddDisabled(true)
+            setAddState(true);
+        }
+    }
+
+    const handleEditButton = () => {
+        if (editDisabled) {
+            setEditState(false);
+            setEditDisabled(false)
+        } else {
+            setEditDisabled(true);
+            setEditState(true);
+        }
     }
 
     const addData = () => {
@@ -94,41 +123,94 @@ export const Entries = props => {
         setAddState(false);
     }
 
+    const editData = () => {
+        fetch(url, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'data': listData })
+        })
+
+        setEditDisabled(false);
+        setEditState(false);
+    }
+
+    const cancelAdd = () => {
+        setAddDisabled(false);
+        setTypedData({ content: '' });
+        setAddState(false);
+    }
+
+    const cancelEdit = () => {
+        setEditDisabled(false);
+        setEditState(false);
+    }
+
   return (
     <div>
         {
             listData ?
 
             <div>
+                <div className='w-[94%] flex justify-end text-2xl text-zinc-500'>
+                    <button onClick={handleEditButton}>
+                        {editDisabled ? 'cancel' : 'Edit'}
+                    </button>
+                </div>
+
                 {listData && listData.map(({ id, content, checked }) => (
                     <label id="content" key={id}>
-                        <input type="checkbox" data-id={id} defaultChecked={checked} onClick={editChecked}/>
-                        <div id='entry__container' className='w-11/12 inline-flex justify-between'>
-                            <div className={`text ${!darkMode && 'textLight'}`}>
-                                {content}
+                        {!editState ? 
+                            <>
+                                <input type="checkbox" data-id={id} defaultChecked={checked} onClick={editChecked}/>
+                                <div id='entry__container' className='w-11/12 inline-flex justify-between'>
+                                    <div className={`text ${!darkMode && 'textLight'}`}>
+                                        {content}
+                                    </div>
+                                </div>
+                                
+                                <div className='inline-block'>
+                                    <button className='text-red-700 hover:text-red-600' data-id={id} onClick={handleDelete}>X</button>
+                                </div>
+                            </>
+
+                            :
+
+                            <div className={`text ${!darkMode} && 'textLight w-11/12`}>
+                                <input
+                                    type="text"
+                                    data-id={id}
+                                    className='w-11/12 bg-transparent outline-none px-2'
+                                    value={content}
+                                    onChange={changeEditInput}
+                                    onKeyPress={(e) => e.key === 'Enter' && editData()}
+                                    onKeyDown={(e) => e.keyCode === 27 && cancelEdit()}
+                                    autoFocus
+                                />
                             </div>
-                        </div>
-                        
-                        <div className='inline-block'>
-                            <button className='text-zinc-500 px-1'>Edit</button>
-                            <button className='text-red-700 hover:text-red-600 px-1' data-id={id} onClick={handleDelete}>X</button>
-                        </div>
+                        }
                     </label>
                 ))}
 
                 {addState && <div id='content' className={`text ${!darkMode} && 'textLight`}>
                     <input
                         type="text"
-                        className='w-11/12 bg-transparent rounded-md outline-none px-2'
+                        className='w-11/12 bg-transparent outline-none px-2'
                         value={typedData.content}
                         onChange={changeInput}
                         onKeyPress={(e) => e.key === 'Enter' && addData()}
+                        onKeyDown={(e) => e.keyCode === 27 && cancelAdd()}
                         autoFocus
                     />
                 </div>}
 
-                <div className='w-full flex justify-end px-16 text-white text-4xl'>
-                    {!addDisabled && <button onClick={handleAddButton}>+</button>}
+                <div className='w-[93%] flex justify-end text-white'>
+                    {!editState && 
+                        <button onClick={handleAddButton}>
+                            {addDisabled ? 'cancel' :  <FiPlus />}
+                        </button>
+                    }
                 </div>
             </div>
             
